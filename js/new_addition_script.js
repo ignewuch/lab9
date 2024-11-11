13cbe6ee3c2a51346e39acee81f53d70ee7374f1
ignewuch <ignewuch@hawk.iit.edu>
2024-11-10 20:24:03 -0600

    updates to java code and index code so they work together better

--- js/new_addition_script.js
+++ js/new_addition_script.js
@@ -2,14 +2,22 @@ function init() {
   var button = document.getElementById('entrybutton');
 
   button.addEventListener('click', showMeText);
+  
+  button.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      showMeText();
+    }
+  });
 }
 
 function showMeText() {
   var textbox = document.getElementById('entryinput');
 
-  document.getElementById('textoutput').innerHTML = textbox.value;
+  var headline = document.getElementById('textoutput');
 
   alert("Isabel Gnewuch: " + textbox.value);
+
+  headline.innerHTML = textbox.value;
 }
 
 window.addEventListener('load', init);
